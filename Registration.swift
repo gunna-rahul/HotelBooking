7e5a51f8301a8072988d898ecb04dcb26d54f851
gunna-rahul <grk25303@gmail.com>
2024-01-24 22:35:18 +0530

    Initial Commit

---
+++ Registration.swift
@@ -0,0 +1,20 @@
+//
+//  Registration.swift
+//  HotelBooking
+//
+//  Created by user2 on 24/01/24.
+//
+
+import Foundation
+struct Registration{
+    var firstName:String
+    var lastName:String
+    var emailAddress:String
+    var checkInDate:Date
+    var chekOutDate:Date
+    var numberOfAdults:Int
+    var numberOfChildrens:Int
+    var Wifi:Bool
+    var roomType:RoomType
+    
+}
